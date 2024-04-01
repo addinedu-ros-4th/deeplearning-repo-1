758ecc8d0c5ec509ccb8bc8907ef625cab5259c1
seungyeonSeok <162243509+seungyeonSeok@users.noreply.github.com>
2024-04-01 12:19:31 +0900

    Add files via upload

--- main/src/workerAssistant.py
+++ main/src/workerAssistant.py
@@ -1,12 +1,14 @@
 import os
 import sys
 import time 
+import cv2
+import numpy as np 
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtGui import *
+from PyQt5.QtCore import QThread, pyqtSignal, Qt, QTimer
 from read_xml import Cxml_reader
 from connect_database import Cdatabase_connect
-
 
 
 form_loginpage_ui = uic.loadUiType("loginWindow.ui")[0]
@@ -16,6 +18,8 @@ form_errorwindowpage_ui = uic.loadUiType("errorWindow.ui")[0]
 form_statisticpage_ui = uic.loadUiType("statisticWindow.ui")[0]
 form_servicenotready_ui = uic.loadUiType("serviceNotReady.ui")[0]
 
+inputID=''; name='' ; #사용자 정보를 받아와서 전역변수화 시키는건 어떨까요 제 컴에서는 name이 로딩이 안돼서 이걸로 띄웠습니다 
+
 class MainWindow(QMainWindow, form_loginpage_ui):
     def __init__(self):
         super().__init__()
@@ -23,6 +27,7 @@ class MainWindow(QMainWindow, form_loginpage_ui):
         self.loginButton.clicked.connect(self.get_login)
         self.login.hide()
         self.IDinput.textChanged.connect(self.check_text)
+        
         #현재 operator id 받아오기 
         self.currentOperator = 0
         self.operatorList = self.get_operatorList()
@@ -33,7 +38,6 @@ class MainWindow(QMainWindow, form_loginpage_ui):
             self.IDinput.setText("")
             self.login.hide()
 
-
     def get_operatorList(self):
         db = Cdatabase_connect()
         result = db.get_operator()
@@ -41,12 +45,15 @@ class MainWindow(QMainWindow, form_loginpage_ui):
         return result
     
     def check_text(self):
+        global inputID, name 
         id_input = self.IDinput.text()
-        
         for val in self.operatorList:
             if id_input == str(val[0]):
                 self.login.show()
-                self.login.setText("ID : %s 작업자 %s님 작업장으로 로그인 합니다"%(id_input,val[1])) 
+                inputID=val[0]
+                name = val[1]                
+                self.login.setText("ID : %s 작업자 %s님 작업장으로 로그인 합니다"%(inputID,name)) 
+
             elif id_input == "":
                 self.login.hide()
 
@@ -96,10 +103,31 @@ class selectWindow(QMainWindow,form_selectpage_ui):
         statisticPage = statisticWindow(self) #페이지 4 불러오고
         statisticPage.get_currentoperator(self.currentOperator)
         statisticPage.show()  
+
+# 웹캠 송출 클래스 
+class CameraThread(QThread):
+    frame_ready = pyqtSignal(np.ndarray)
+
+    def __init__(self, camera_id):
+        super().__init__()
+        self.camera_id = camera_id
+        self.running = False
+
+    def run(self):
+        self.running = True
+        self.video = cv2.VideoCapture(self.camera_id)
+        while self.running:
+            retval, frame = self.video.read()
+            if retval:
+                self.frame_ready.emit(frame)
+    def stop(self):
+        self.running = False
+        self.wait() 
+       
         
-        
-class assemblyWindow(QMainWindow,form_assemblypage_ui):
+class assemblyWindow(QMainWindow,form_assemblypage_ui): #-- 해결해야하는 문제: back이나 로그아윳을하면 카메라 연동이 안됨 
     def __init__(self, parent):
+        global inputID, name 
         super().__init__(parent)
         self.setupUi(self) 
 
@@ -113,9 +141,50 @@ class assemblyWindow(QMainWindow,form_assemblypage_ui):
         self.backButton.clicked.connect(self.go_back)
         self.errorButton.clicked.connect(self.go_error)
         self.operatorList = self.get_operatorList()
+        self.idLabel.setText(inputID)
+        self.nameLabel.setText(name)
+                
+    # ---- 캠 송출 코드 
+        self.label1 = self.workNowLabel
+        self.label2 = self.materialLabel
+        
+        available_index = []
+        for index in range(5):  # 임의의 범위를 지정하여 카메라 인덱스를 확인
+            camera = cv2.VideoCapture(index)
+            if camera.isOpened():
+                available_index.append(index)
+                camera.release()        
+
+        self.camera1_thread = CameraThread(available_index[0]) 
+        self.camera1_thread.frame_ready.connect(self.update_frame1)
+        self.camera1_thread.start()
+        self.camera2_thread = CameraThread(available_index[1]) 
+        self.camera2_thread.frame_ready.connect(self.update_frame2)
+        self.camera2_thread.start()
+            
+    def update_frame1(self, frame):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        height, width, channel = frame.shape
+        bytes_per_line = 3 * width
+        q_img = QPixmap.fromImage(QImage(frame.data, 391, 421, bytes_per_line, QImage.Format_RGB888))
+        self.label1.setPixmap(q_img.scaled(self.label1.width(), self.label1.height(), Qt.KeepAspectRatio))
+
+    def update_frame2(self, frame):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        height, width, channel = frame.shape
+        bytes_per_line = 3 * width
+        q_img = QPixmap.fromImage(QImage(frame.data, 371, 421, bytes_per_line, QImage.Format_RGB888))
+        self.label2.setPixmap(q_img.scaled(self.label2.width(), self.label2.height(), Qt.KeepAspectRatio))
+
+    def closeEvent(self, event):
+        self.camera1_thread.stop()
+        self.camera2_thread.stop()
+        event.accept() 
+    ## ------캠 송출 코드     
     
     def get_currentoperator(self,id):
         self.currentOperator = id
+        
 
     def get_operatorList(self):
         db = Cdatabase_connect()
@@ -130,12 +199,12 @@ class assemblyWindow(QMainWindow,form_assemblypage_ui):
         for val in self.operatorList:
             if self.currentOperator == val[0]:
                 self.nameLabel.setText(val[1])
+                
 
         for idx, val in enumerate(self.workorderlist):
             tmp_txt = "{}. ".format(idx+1) +val
             self.progresslist[idx].setText(tmp_txt)
-            
-        
+                 
     def go_main(self):
         self.hide()
         self.parent().parent().get_currentoperator(0)
@@ -143,7 +212,7 @@ class assemblyWindow(QMainWindow,form_assemblypage_ui):
            
     def go_back(self):
         self.hide() #현재 화면 숨겨주고
-        self.parent().show() #작업페이지로 감 
+        self.parent().show() #작업선택 페이지로 감 
 
     def go_error(self):
         self.hide() #현재 화면 숨겨주고
@@ -155,8 +224,12 @@ class errorWindow(QMainWindow,form_errorwindowpage_ui):
     def __init__(self, parent):
         super().__init__(parent)
         self.setupUi(self)
+        global inputID, name 
+        self.idLabel.setText(inputID)
+        self.nameLabel.setText(name)  
         self.saveButton.clicked.connect(self.report_error)
         self.operatorList = self.get_operatorList()
+        self.backButton.clicked.connect(self.go_back)
 
     def showEvent(self,event):
         super().showEvent(event)
@@ -171,7 +244,10 @@ class errorWindow(QMainWindow,form_errorwindowpage_ui):
         result = db.get_operator()
         db.dbclose()
         return result
-
+    def go_back(self):
+        self.hide() #현재 화면 숨겨주고
+        self.parent().show() #작업페이지로 감 
+        
     def report_error(self): #에러나면 어디로 가지? 일단 뒤로 
         self.hide() #현재 화면 숨겨주고
         self.parent().show() #작업페이지로 감 
@@ -183,10 +259,12 @@ class statisticWindow(QMainWindow,form_statisticpage_ui):
    
     def __init__(self, parent):
         super().__init__(parent)
+        global inputID, name 
         self.setupUi(self)
         self.logoutButton.clicked.connect(self.go_main)
         self.backButton.clicked.connect(self.go_back)
         self.operatorList = self.get_operatorList()
+      
 
     def showEvent(self,event):
         super().showEvent(event)
