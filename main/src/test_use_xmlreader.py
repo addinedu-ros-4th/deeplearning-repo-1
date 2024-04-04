0d98403ba2837c7c985ceb5f42687fd8d0cb4e7e
dyjung <dyjung.dev@gmail.com>
2024-04-04 15:32:16 +0900

    xml reader add object detectionlist xml

--- main/src/test_use_xmlreader.py
+++ main/src/test_use_xmlreader.py
@@ -11,5 +11,17 @@ def main():
     #xml안에 들어 있는 작업순서(string)가 리스트 형태로 출력된다
     print(cxml.get_order_list())
 
+    #xml_reader 클래스를 생성한다. 생성시 불러올 xml 주소를 인자로 넘겨준다
+    cxml_objectdetect = Cxml_reader("objectdetectlist.xml", "dog_light")
+    
+    #xml안에 들어 있는 작업 순서 갯수 출력 
+    print(cxml_objectdetect.get_model_list())
+    
+    #xml안에 들어 있는 작업순서(string)가 리스트 형태로 출력된다
+    print(cxml_objectdetect.get_object_count_list())
+
+    #xml안에 들어 있는 작업순서(string)가 리스트 형태로 출력된다
+    print(cxml_objectdetect.get_object_parts_list())
+
 if __name__ == "__main__":
     main()
