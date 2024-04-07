510e484a817a6c492df5e73aa5c1bb557a28d583
seungyeonSeok <162243509+seungyeonSeok@users.noreply.github.com>
2024-04-08 01:30:41 +0900

    Add files via upload

--- main/src/connect_database.py
+++ main/src/connect_database.py
@@ -7,8 +7,8 @@ class Cdatabase_connect:
         self.local = mysql.connector.connect(
             host = "localhost",
             port = 3306,
-            user = "dyjung",
-            password = "0",
+            user = "root",
+            password = "0000",
             database = "mlproject" 
         )
 
@@ -27,7 +27,7 @@ class Cdatabase_connect:
 
         return result_list
 
-    def get_workdata(self):
+    def get_workdatas(self): #workdata 불러오기 
         result_list = []
         cursor = self.local.cursor()
 
@@ -42,16 +42,40 @@ class Cdatabase_connect:
             
         return result_list
 
-    def insert_workdata(self,work_id, operator_id, workdone_time, status):
+    def get_errordata(self): #errordata불러오기 
+        result_list = []
+        cursor = self.local.cursor()
+
+        # 쿼리 실행
+        sql = "SELECT * FROM errordata;"
+        
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
+        for val in result:
+            result_list.append(list(val))
+            
+        return result_list
+
+    def insert_workdata(self,work_name, operator_id, operator_name,workdone_time, status): #workdata 업데이트 
 
         cursor = self.local.cursor()
         # 삽입할 데이터
-        data = (work_id,operator_id,workdone_time,status)
+        data = (work_name, operator_id, operator_name,workdone_time, status)
 
         # 쿼리 실행
-        sql = "INSERT INTO workdata (work_id, operator_id, workdone_time, status) VALUES (%s, %s, %s, %s)"
+        sql = "INSERT INTO workdata (work_name, operator_id, operator_name,workdone_time, status) VALUES (%s, %s, %s, %s,%s)"
         cursor.execute(sql,data)
         self.local.commit()
+        
+    def insert_errordata(self,work_name,ID,name, work_time,error_part,error_reason): #errordata업데이트
+        cursor = self.local.cursor()
+        # 삽입할 데이터
+        data = (work_name,ID,name, work_time,error_part,error_reason)    
+        # 쿼리 실행
+        sql = "INSERT INTO errordata (work_name,operator_id,operator_name, workdone_time,error_part,error_reason) VALUES (%s, %s, %s, %s, %s, %s)"
+        cursor.execute(sql,data)
+        self.local.commit()            
 
     def dbclose(self):
         self.local.close()
